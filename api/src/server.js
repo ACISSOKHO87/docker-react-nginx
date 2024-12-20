45905f5fbc4a1ab3d253f7bf15e8a1aa3b8940cf
Abdoulaye CISSOKHO <ab.cissokho87@gmail.com>
2024-12-20 10:55:12 +0100

    add modify

--- api/src/server.js
+++ api/src/server.js
@@ -1,39 +1,46 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-const signals = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const signals = ["SIGINT", "SIGTERM", "SIGQUIT"];
 let count;
-const mongoUrl = process.env.NODE_ENV === 'production' ?
-`mongodb://${ process.env.MONGO_USERNAME }:${ process.env.MONGO_PWD }@db` :
-`mongodb://db`;
+const mongoUrl =
+    process.env.NODE_ENV === "production"
+        ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@db`
+        : `mongodb://db`;
 
-const client = new MongoClient(mongoUrl); 
-async function run() { 
-  try {
-    await client.connect();
-    await client.db('admin').command({ ping: 1 });
-    console.log('CONNEXION DB OK !');
-    count = client.db('test').collection('count');
-    if ((await count.countDocuments()) === 0) {
-        count.insertOne({ count: 0 });
+const client = new MongoClient(mongoUrl);
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("CONNEXION DB OK !");
+        count = client.db("test").collection("count");
+        if ((await count.countDocuments()) === 0) {
+            count.insertOne({ count: 0 });
+        }
+    } catch (err) {
+        console.log(err.stack);
     }
-  } catch (err) {
-    console.log(err.stack);
-  }
 }
 run().catch(console.dir);
 
-
 const app = express();
 
-app.get('/api/count', (req, res) => {
-  count.findOneAndUpdate({}, { $inc: { count: 1 } }, { returnNewDocument: true, upsert: true }).then((doc) => {
-    res.status(200).json(doc ? doc.count : 0);
-  })
-})
+app.get("/api/count", (req, res) => {
+    count
+        .findOneAndUpdate(
+            {},
+            { $inc: { count: 1 } },
+            { returnNewDocument: true, upsert: true }
+        )
+        .then((doc) => {
+            const test = doc ? doc.count : 0;
+            res.status(200).json(test);
+        });
+});
 
-app.all('*', (req, res) => {  
-  res.status(404).end();
-})
+app.all("*", (req, res) => {
+    res.status(404).end();
+});
 
 const server = app.listen(80);
 
@@ -42,18 +49,20 @@ const server = app.listen(80);
 //On empêche les nouvelles connexions sur le serveur
 //Ensuite on close proprement la connexion DB
 
-signals.forEach(signal => process.on('SIGINT', () => {
-    server.close((err) => {
-        if (err) {
-            process.exit(1);
-        } else {
-            if (client) {
-                client.close((err) => {
-                process.exit(err ? 1 : 0); 
-            });
+signals.forEach((signal) =>
+    process.on("SIGINT", () => {
+        server.close((err) => {
+            if (err) {
+                process.exit(1);
             } else {
-                process.exit(0);
+                if (client) {
+                    client.close((err) => {
+                        process.exit(err ? 1 : 0);
+                    });
+                } else {
+                    process.exit(0);
+                }
             }
-        } 
-    });
-}))
+        });
+    })
+);
--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState();
+    const [count, setCount] = useState();
 
-  const fetchCount = async() => {
-    try {
-      const response = await fetch('/api/count');
-      if (response.ok) {
-        setCount(await response.json());
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  useEffect(() => {
-    
-    fetchCount();
-  },[]);
+    const fetchCount = async () => {
+        try {
+            console.log("cououc");
+            const response = await fetch("/api/count");
+            console.log("resp:", response);
+            const respjson = await response.json();
+            console.log(respjson);
+            if (response.ok) {
+                const test = await response.json();
+                console.log(test);
+                setCount(await response.json());
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    useEffect(() => {
+        fetchCount();
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to update and reload.
-        </p>
-        <p>Count: { count }</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to update and reload.
+                </p>
+                <p>Count: {count}</p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
